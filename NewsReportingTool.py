4b22f06650a053651d0812b3e2a31d63fe6d9644
BePitch <bpitchfo@gmail.com>
2018-06-04 17:34:38 -0400

    Updates
    from reviewer comments

--- NewsReportingTool.py
+++ NewsReportingTool.py
@@ -1,65 +1,62 @@
+#!/usr/bin/env python3
 # Database code for the News DataBase .
 import psycopg2
 from datetime import datetime
-
-#Databse global variable
+# Databse global variable
 DBNAME = "news"
 
-#Build out the use for returning the articles report
+
+# Build out the use for returning the articles report
 def report_articles():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute(open("topViewArticles.sql","r").read())
-  reportArticles = c.fetchall()
-    #build output format
-  print("Top 3 articles viewed all time")
-  print("\n")
-  print("Article Name      |     Views    ")
-  for i in reportArticles:
-    print(str(i[1]) + ' -- ' + str(i[2]))
-  print("\n")
-  db.close()
-  return reportArticles
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(open("topViewArticles.sql", "r").read())
+    reportArticles = c.fetchall()
+    # build output format
+    print("Top 3 articles viewed all time")
+    print("\n")
+    print("Article Name      |     Views    ")
+    for i in reportArticles:
+        print(str(i[1]) + ' -- ' + str(i[2]))
+    print("\n")
+    db.close()
+    return reportArticles
 
-#Build out the use for returning the author report
+
+# Build out the use for returning the author report
 def report_authors():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute(open("topAuthors.sql","r").read())
-  reportAuthors = c.fetchall()
-    #build output format
-  print("Top authors viewed all time")
-  print("\n")
-  print("Author Name    |   Views  ")
-  for i in reportAuthors:
-    print(str(i[0]) + ' -- ' + str(i[1]))
-  print("\n")
-  db.close()
-  return reportAuthors
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(open("topAuthors.sql", "r").read())
+    reportAuthors = c.fetchall()
+    # build output format
+    print("Top authors viewed all time")
+    print("\n")
+    print("Author Name    |   Views  ")
+    for i in reportAuthors:
+        print(str(i[0]) + ' -- ' + str(i[1]))
+    print("\n")
+    db.close()
+    return reportAuthors
 
-#Build out the use for returning the error report
+
+# Build out the use for returning the error report
 def report_errors():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute(open("errorsDaily.sql","r").read())
-  reportErrors = c.fetchall()
-    #build output format
-  print("Days with an error rate >= 1%")
-  print("\n")
-  print("Error Date    | Rate of error  ")
-  for i in reportErrors:
-    print((i[0]).strftime('%B %d, %Y') + ' | ' + str(i[4])+ '%')
-  print("\n")
-  db.close()
-  return reportErrors
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(open("errorsDaily.sql", "r").read())
+    reportErrors = c.fetchall()
+    # build output format
+    print("Days with an error rate >= 1%")
+    print("\n")
+    print("Error Date    | Rate of error  ")
+    for i in reportErrors:
+        print((i[0]).strftime('%B %d, %Y') + ' | ' + str(i[4]) + '%')
+    print("\n")
+    db.close()
+    return reportErrors
 
 if __name__ == "__main__":
     report_articles()
     report_authors()
     report_errors()
-    
-
-
--- README.txt
+++ README.txt
@@ -1,5 +1,13 @@
---Several files in folder but for this project this is the description
+--Original Date: 2018-06-01
+--Updated Date: 2018-06-04
+--Project: Udacity FSND Logs Analysis Project
+--Python Version 3.6
+--Vagrant 2.1.1
 
+tldr; Run Python app: While in vagrant dir, i run command [ python NewsReportingTool.py > output.txt] to kick off my python file which will output to a flat file
+
+--Several files in folder but for this project this is the description
+Files:
 1. README.txt = instructions file explaining project
 2. FileOutput.png - screenshot of vagrant output of answers to questions
 3. NewsReportingTool.py = python file to run sql queries and output answers to questions
@@ -7,3 +15,14 @@
     a. topViewArticles.sql = query that returns the top 3 viewed articles 
     b. topAuthors.sql = query that returns top authors by views for all time
     c. errorsDaily.sql = query that returns total views, total success, total errors then calculates error rate
+
+Program run:
+1. I 'cd' into my working directory using GIT Bash
+2. Run command vagrant up
+3. Run command vagrant ssh
+4. Now that I am in vagrant I cd into vagrant/newsdata/FSND_LogsProject folder
+5. If I want to explore my News database I run psql News
+    a. I can \d or \dt to explore tables in database and see columns
+    b. \q to exit
+6. Run Python app: While in vagrant dir, i run python NewsReportingTool.py to kick off my python file which will output in the terminal display the results of the query
+    or run [ python NewsReportingTool.py > output.txt] to output to a file.
