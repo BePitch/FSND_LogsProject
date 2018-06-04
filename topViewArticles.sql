69781ea91c558e29451b462eade8f6db36704f3f
BePitch <bpitchfo@gmail.com>
2018-06-04 17:38:37 -0400

    Another One
    per comments

--- topViewArticles.sql
+++ topViewArticles.sql
@@ -1,5 +1,5 @@
 Select 
-		a.path,
+		--a.path,
 		b.slug,
 		count(*) as total
 		
@@ -8,7 +8,7 @@ from log a
 inner join articles b on concat('/article/',b.slug) = a.path
 
 	--and b.id = 27
-group by a.path,
+group by --a.path,
 		b.slug
 order by total desc
 limit 3;
